7c8891b95297a7d85438afdc4ce0b0dda5563bd9
kruthardh <krpd18@student.bth.se>
2020-08-07 09:17:52 +0200

    pushind median and its test script

--- test.py
+++ test.py
@@ -9,47 +9,27 @@ import statistics as stat
 
 X = [1, 1.2 ,3 ,-5,0,100]
 
-main_mean    = []
-main_avg     = []
-main_average = []
+main_median    = []
 
-test_mean    = []
-test_avg     = []
-test_average = []
+
+test_medan    = []
 
 
 
-test_mean.append(stat.mean(X))
-test_avg.append(stat.mean(X))
-test_average.append(stat.mean(X))
+
+test_median.append(stat.median(X))
 
 
 
-r1 = requests.get('http://127.0.0.1:5000/mean?X=1,1.2,3,-5,0,100')
-data1= r1.json()
-main_mean.append(data1)
 
-r2 = requests.get('http://127.0.0.1:5000/avg?X=1,1.2,3,-5,0,100')
-data1= r2.json()
-main_avg.append(data1)
-
-r3 = requests.get('http://127.0.0.1:5000/average?X=1,1.2,3,-5,0,100')
-data1= r3.json()
-main_average.append(data1)
+r4 = requests.get('http://127.0.0.1:5000/median?X=1,1.2,3,-5,0,100')
+data1= r4.json()
+main_medan.append(data1)
 
 
-if main_mean == test_mean:
-	print ("Mean OK")
+
+if main_median == test_median:
+	print ("Median OK")
 else:
-	print ("Mean not OK")
-
-if main_avg == test_avg:
-	print ("Avg OK")
-else:
-	print ("Avg not OK")
-
-if main_average == test_average:
-	print ("Average OK")
-else:
-	print ("Average not OK")
+	print ("Median not OK")
 
